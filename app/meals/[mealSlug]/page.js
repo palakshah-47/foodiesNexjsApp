ba7e5e8b5b83526b3e652f12aff8cf56759aa08a
palakshah-47 <palakshah47@gmail.com>
2024-11-07 07:47:28 -0500

    changes for header footer and community

---
+++ app/meals/[mealSlug]/page.js
@@ -0,0 +1,5 @@
+const MealDetailsPage = () => {
+  return <h1>Meal DetailsPage</h1>;
+};
+
+export default MealDetailsPage;
---
+++ components/main-header/main-header.js
@@ -0,0 +1,29 @@
+import logoImg from '@/assets/logo.png';
+import Link from 'next/link';
+import classes from './main-header.module.css';
+import Image from 'next/image';
+import MainHeaderBackGround from './main-header-background';
+import NavLink from './nav-link';
+export default function MainHeader() {
+  return (
+    <>
+      <MainHeaderBackGround />
+      <header className={classes.header}>
+        <Link href="/" className={classes.logo}>
+          <Image src={logoImg} alt="A plate with food on it" priority />
+          NextLevel Food
+        </Link>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <NavLink href="/meals">Browse Meals</NavLink>
+            </li>
+            <li>
+              <NavLink href="/community">Foodies community</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
